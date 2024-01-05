66956a8e2b10f54992094eb6c6e19d43f41957ef
miriyala srikanth <123526232+srikanthmiriyala017@users.noreply.github.com>
2024-01-05 15:46:40 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 const Header = () => (
   <div className="header">
-    <h1 className="name">Madelyn Torff</h1>
+    <h1 className="name">Miryala Srikanth</h1>
     <ul>
       <li>
         <Link to="/">
